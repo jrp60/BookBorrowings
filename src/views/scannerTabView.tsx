67dfda355ec98dbe0c0fcafde0d2fd30e09bbbf3
jrp60 <javirodenas.javirodenas@gmail.com>
2022-09-01 14:40:34 +0200

    back4app get and set, conditional rendering button

--- src/views/scannerTabView.tsx
+++ src/views/scannerTabView.tsx
@@ -2,17 +2,46 @@ import React, {useState} from 'react';
 import {Text, StyleSheet, TouchableOpacity} from 'react-native';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 import {RNCamera} from 'react-native-camera';
+import Parse from 'parse/react-native';
 
 const ScannerTabView = () => {
   const [isbn, setIsbn] = useState('');
+  const [showBottomContent, setShowBottomContent] = useState(false);
   const getISBN = (isbn: string) => {
     console.log('ISBN leido: ', isbn);
     setIsbn(isbn);
   };
 
+  const readIsbn = (isbn: string) => {
+    setIsbn(isbn);
+    console.log('ISBN leido: ', isbn);
+    setShowBottomContent(true);
+  };
+
+  const [person, setPerson] = useState(new Parse.Object('Person'));
+  async function fetchPerson() {
+    //create your Parse Query using the Person Class you've created
+    let query = new Parse.Query('Person');
+    //run the query to retrieve all objects on Person class, optionally you can add your filters
+    let queryResult = await query.findAll();
+    //pick the first result
+    const currentPerson = queryResult[0];
+    //access the Parse Object attributes
+    console.log('person id: ', currentPerson.get('id'));
+    console.log('person name: ', currentPerson.get('name'));
+    console.log('person email: ', currentPerson.get('email'));
+    setPerson(currentPerson);
+  }
+  const openBook = () => {
+    console.log('ISBN: ', isbn);
+    console.log('person: ', person);
+    console.log('And now fetch!');
+    fetchPerson();
+  };
+
   return (
     <QRCodeScanner
-      onRead={e => getISBN(e.data)}
+      onRead={e => readIsbn(e.data)}
       fadeIn={true}
       reactivate={true}
       reactivateTimeout={3000}
@@ -27,11 +56,13 @@ const ScannerTabView = () => {
           Sitúe el código de barras en el campo de la cámara
         </Text>
       }
-      bottomContent={
-        <TouchableOpacity style={styles.buttonTouchable}>
-          <Text style={styles.buttonText}></Text>
-        </TouchableOpacity>
-      }
+      {...(showBottomContent && {
+        bottomContent: (
+          <TouchableOpacity style={styles.buttonTouchable} onPress={openBook}>
+            <Text style={styles.buttonText}>Ver Libro</Text>
+          </TouchableOpacity>
+        ),
+      })}
     />
   );
 };
